c3c078356339aa3451357b1c13b0c148b9fbf8a6
kylix.par <minhvcth1806008@fpt.edu.vn>
2019-12-13 20:45:55 +0700

    client

---
+++ ConsumingRESTServiceCRUD_Client/Controllers/BookController.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using ConsumingRESTServiceCRUD_Client.Models;
+using Microsoft.Ajax.Utilities;
+
+namespace ConsumingRESTServiceCRUD_Client.Controllers
+{
+    public class BookController : Controller
+    {
+        // GET: Book
+        public ActionResult Index()
+        {
+            
+            BookServiceClient bsc = new BookServiceClient();
+            ViewBag.listBooks = bsc.getAllBook();
+            return View();
+        }
+    }
+}
---
+++ ConsumingRESTServiceCRUD_Client/Models/BookServiceClient.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Web;
+using System.Web.Script.Serialization;
+
+namespace ConsumingRESTServiceCRUD_Client.Models
+{
+    public class BookServiceClient
+    {
+        private String BASE_URL = "http://localhost:55133/BookServices.svc/";
+        // I just call one method "GetAllBooks" only
+        public List<Book> getAllBook()
+        {
+            var syncClinet = new WebClient();
+            var content = syncClinet.DownloadString(BASE_URL + "Books");
+            var json_serializer = new JavaScriptSerializer();
+            return json_serializer.Deserialize<List<Book>>(content);
+        }
+    }
+}
